2c2fe4b7e20060fcb4cc083ae3be4da3197d84c6
sayakboi <sayaknandi.181me174@nitk.edu.in>
2021-04-07 22:13:37 +0530

    added graphs

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 //variable declaration
 //          inputs
 var m = 1 ;
-var c = 1;
+var dr = 1;
 var k = 1;
 var amp = 1;
 var fw = new Array(n);
@@ -17,8 +17,9 @@ var dt = 0.05;
 var wn;
 var wd;
 var cc;
-var dr;
+var c;
 var tr = new Array(n*10);
+var phse = new Array(n*10);
 var q;
 var freqr = new Array(n*10);
 
@@ -27,7 +28,7 @@ function assign()
 {
   m = +document.getElementById('mass').value || 1.00;
   k = +document.getElementById('k').value || 1.00;
-  c = +document.getElementById('dc').value || 1.00;
+  dr = +document.getElementById('dr').value || 1.00;
   amp = +document.getElementById('baseamp').value || 1.00;
   ff = +document.getElementById('Omega').value || 1.00;
   for (i = 0; i<n; i++)
@@ -42,7 +43,7 @@ function compute()
   assign();
 
   outputVals();
-  //plot();
+  plot();
 }
 function outputVals()
 {
@@ -50,17 +51,90 @@ function outputVals()
   document.getElementById('NAF').innerHTML = wn;
   cc = 2*m*wn;
   document.getElementById('CC').innerHTML = cc;
-  dr = c/cc;
-  document.getElementById('DR').innerHTML = dr;
+  c = dr*cc;
+  document.getElementById('C').innerHTML = c;
 
   wd = wn*Math.sqrt(1-dr*dr);
   document.getElementById('DAF').innerHTML = wd;
 
   var trs = transmissibility(ff/wn);
   document.getElementById('TR').innerHTML = trs;
+
+  var ph = phase(ff/wn);
+  document.getElementById('PH').innerHTML = ph;
+  for (i = 0; i<n*10; i++)
+  {
+    freqr[i] = i*dt*10/wn;
+    tr[i] = transmissibility(freqr[i]);
+    phse[i] = phase(freqr[i]);
+  }
 }
 function transmissibility(frr)
 {
   var trs = Math.sqrt((1+Math.pow((2*dr*frr),2))/(Math.pow((1-Math.pow(frr,2)),2)+Math.pow((2*dr*frr),2)));
   return trs;
+}
+function phase(frr)
+{
+  var phase_ = Math.atan2((-2*dr*(Math.pow(frr,3)))/(1-(1-4*(Math.pow(dr,2)))*((Math.pow(frr,2)))));
+  return phase_;
+}
+function plot()
+{
+  var trace = {
+          x: freqr,
+          y: tr,
+          type: 'scatter',
+          mode : 'lines'};
+  var layout = {
+        title: {
+          text:'Transmissibility vs Frequency Ratio',
+          font: {
+          family: 'Courier New, monospace',
+          size: 24
+        }},
+        xaxis: {
+          type: 'log',
+          title:{
+          text: 'Frequency Ratio',
+          font: {
+            family: 'Courier New, monospace',
+            size: 18,
+            color: '#7f7f7f'}},
+          autorange: true
+        },
+        yaxis: {
+          type: 'log',
+          title:{text: 'Transmissibility',font: {  family: 'Courier New, monospace',  size: 18, color: '#7f7f7f'}},
+          autorange: true
+        }};
+        TRFR= document.getElementById('trvsfreqr');
+        Plotly.newPlot( TRFR, [trace], layout);
+  var trace = {
+          x: freqr,
+          y: phse,
+          type: 'scatter',
+          mode : 'lines'};
+  var layout = {
+        title: {
+          text:'Phase angle vs Frequency Ratio',
+          font: {
+          family: 'Courier New, monospace',
+          size: 24
+        }},
+        xaxis: {
+          title:{
+          text: 'Frequency Ratio',
+          font: {
+            family: 'Courier New, monospace',
+            size: 18,
+            color: '#7f7f7f'}},
+          autorange: true
+        },
+        yaxis: {
+          title:{text: 'Phase angle',font: {  family: 'Courier New, monospace',  size: 18, color: '#7f7f7f'}},
+          autorange: true
+        }};
+        TRFR= document.getElementById('phvsfreqr');
+        Plotly.newPlot( TRFR, [trace], layout);
 }
